d35f29d6f19616b0a0fb9f8ae750e1f425ff1cdb
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-04-03 20:54:38 +0900

    [WIP]ContainerViewサンプルのプロジェクトの生成

---
+++ ContainerViewSample/ContainerViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContainerViewSample
+//
+//  Created by 酒井文也 on 2016/04/03.
+//  Copyright © 2016年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
