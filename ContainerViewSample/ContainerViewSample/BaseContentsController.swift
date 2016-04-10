03e79cc39dc2e01335d198f5de66b2e5f930aaa7
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-04-11 04:13:26 +0900

    [WIP]ScrollViewとContainerViewの合わせ技（警告でてる...）

--- ContainerViewSample/ContainerViewSample/BaseContentsController.swift
+++ ContainerViewSample/ContainerViewSample/BaseContentsController.swift
@@ -8,28 +8,190 @@
 
 import UIKit
 
-class BaseContentsController: UIViewController {
+//スクロールビューのタグに使用するenum
+enum ScrollViewTag : Int {
+    case MenuScroll = 0
+    case MainScroll
+}
 
+//定数設定などその他
+struct ControllersSettings {
+    
+    //ScrollViewに表示するボタン名称
+    static let pageScrollNavigationList: [String] = [
+        "🔖1番目",
+        "🔖2番目",
+        "🔖3番目",
+        "🔖4番目",
+        "🔖5番目",
+        "🔖6番目"
+    ]
+    
+}
+
+class BaseContentsController: UIViewController, UIScrollViewDelegate {
+
+    //メンバ変数
+    var basePosition: Int! = 1
+    
+    //Outlet接続した部品一覧（ScrollView）
+    @IBOutlet var menuScrollView: UIScrollView!
+    @IBOutlet var mainScrollView: UIScrollView!
+    
+    //ViewControllerをembed segueでつないでいるContainerの配置
+    @IBOutlet var firstContainer: UIView!
+    @IBOutlet var secondContainer: UIView!
+    @IBOutlet var thirdContainer: UIView!
+    @IBOutlet var fourthContainer: UIView!
+    @IBOutlet var fifthContainer: UIView!
+    @IBOutlet var sixthContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        /*
+        self.firstContainer.translatesAutoresizingMaskIntoConstraints = true
+        self.secondContainer.translatesAutoresizingMaskIntoConstraints = true
+        self.thirdContainer.translatesAutoresizingMaskIntoConstraints = true
+        self.fourthContainer.translatesAutoresizingMaskIntoConstraints = true
+        self.fifthContainer.translatesAutoresizingMaskIntoConstraints = true
+        self.sixthContainer.translatesAutoresizingMaskIntoConstraints = true
+        */
     }
     
+    //レイアウト処理が完了した際の処理
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        //スクロールビューの定義
+        self.initMenuScrollViewDefinition()
+        self.initMainScrollViewDefinition()
+        
+        //mainScrollViewの中にContainerを一列に並べて配置する
+        for i in 0...(ControllersSettings.pageScrollNavigationList.count - 1){
+            
+            //各コンテナの位置情報を定義する
+            let pX: CGFloat = CGFloat(Int(self.mainScrollView.frame.width) * i)
+            let pY: CGFloat = CGFloat(0.0)
+            let pW: CGFloat = self.mainScrollView.frame.width
+            let pH: CGFloat = self.mainScrollView.frame.height
+            
+            //各コンテナをコンテンツ用のScrollViewに配置する(もうちょっとエレガントにしたい...)
+            if i == 0 {
+                
+                self.firstContainer.translatesAutoresizingMaskIntoConstraints = true
+                self.firstContainer.frame = CGRectMake(pX, pY, pW, pH)
+                self.mainScrollView.addSubview(self.firstContainer)
+                
+            } else if i == 1 {
 
-    /*
-    // MARK: - Navigation
+                self.secondContainer.translatesAutoresizingMaskIntoConstraints = true
+                self.secondContainer.frame = CGRectMake(pX, pY, pW, pH)
+                self.mainScrollView.addSubview(self.secondContainer)
+                
+            } else if i == 2 {
+                
+                self.thirdContainer.translatesAutoresizingMaskIntoConstraints = true
+                self.thirdContainer.frame = CGRectMake(pX, pY, pW, pH)
+                self.mainScrollView.addSubview(self.thirdContainer)
+                
+            } else if i == 3 {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+                self.fourthContainer.translatesAutoresizingMaskIntoConstraints = true
+                self.fourthContainer.frame = CGRectMake(pX, pY, pW, pH)
+                self.mainScrollView.addSubview(self.fourthContainer)
+                
+            } else if i == 4 {
+                
+                self.fifthContainer.translatesAutoresizingMaskIntoConstraints = true
+                self.fifthContainer.frame = CGRectMake(pX, pY, pW, pH)
+                self.mainScrollView.addSubview(self.fifthContainer)
+                
+            } else if i == 5 {
+                
+                self.sixthContainer.translatesAutoresizingMaskIntoConstraints = true
+                self.sixthContainer.frame = CGRectMake(pX, pY, pW, pH)
+                self.mainScrollView.addSubview(self.sixthContainer)
+
+            }
+            
+            //メニュー用のスクロールビューにボタンを配置
+            let buttonElement: UIButton! = UIButton()
+            self.menuScrollView.addSubview(buttonElement)
+            
+            buttonElement.frame = CGRectMake(
+                CGFloat(Int(self.menuScrollView.frame.width) / 3 * i),
+                CGFloat(0.0),
+                CGFloat(Int(self.menuScrollView.frame.width) / 3),
+                self.menuScrollView.frame.height
+            )
+            buttonElement.backgroundColor = UIColor.clearColor()
+            buttonElement.setTitle(ControllersSettings.pageScrollNavigationList[i], forState: .Normal)
+            buttonElement.titleLabel!.font = UIFont(name: "Bold", size: CGFloat(16))
+            buttonElement.tag = i
+            //buttonElement.addTarget(self, action: #selector(ViewController.buttonTapped(_:)), forControlEvents: .TouchUpInside)
+            
+        }
+        
+        self.mainScrollView.backgroundColor = UIColor.lightGrayColor()
+        self.mainScrollView.contentSize = CGSizeMake(
+            CGFloat(Int(self.mainScrollView.frame.width) * ControllersSettings.pageScrollNavigationList.count),
+            CGFloat(self.menuScrollView.frame.height)
+        )
+        
+        self.menuScrollView.contentSize = CGSizeMake(
+            CGFloat(Int(self.menuScrollView.frame.width) * ControllersSettings.pageScrollNavigationList.count / 3),
+            CGFloat(self.menuScrollView.frame.height)
+        )
+        
     }
-    */
 
+    //Menu用のUIScrollViewの初期化を行う
+    func initMenuScrollViewDefinition() {
+        
+        self.menuScrollView.tag = ScrollViewTag.MenuScroll.rawValue
+        self.menuScrollView.pagingEnabled = false
+        self.menuScrollView.scrollEnabled = true
+        self.menuScrollView.directionalLockEnabled = false
+        self.menuScrollView.showsHorizontalScrollIndicator = false
+        self.menuScrollView.showsVerticalScrollIndicator = false
+        self.menuScrollView.bounces = false
+        self.menuScrollView.scrollsToTop = false
+        
+        //コンテンツサイズの決定
+        self.menuScrollView.contentSize = CGSizeMake(
+            CGFloat(Int(self.menuScrollView.frame.width) * ControllersSettings.pageScrollNavigationList.count / 3),
+            CGFloat(self.menuScrollView.frame.height)
+        )
+        
+    }
+    
+    //Main用のUIScrollViewの初期化を行う
+    func initMainScrollViewDefinition() {
+        
+        self.mainScrollView.tag = ScrollViewTag.MainScroll.rawValue
+        self.mainScrollView.pagingEnabled = true
+        self.mainScrollView.scrollEnabled = true
+        self.mainScrollView.directionalLockEnabled = false
+        self.mainScrollView.showsHorizontalScrollIndicator = true
+        self.mainScrollView.showsVerticalScrollIndicator = false
+        self.mainScrollView.bounces = false
+        self.mainScrollView.scrollsToTop = false
+        
+        //コンテンツサイズの決定
+        self.mainScrollView.backgroundColor = UIColor.lightGrayColor()
+        self.mainScrollView.contentSize = CGSizeMake(
+            CGFloat(Int(self.mainScrollView.frame.width) * ControllersSettings.pageScrollNavigationList.count),
+            CGFloat(self.menuScrollView.frame.height)
+        )
+        
+    }
+    
+    //ハンバーガーボタンを押下した際のアクション
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
--- ContainerViewSample/ContainerViewSample/ViewController.swift
+++ ContainerViewSample/ContainerViewSample/ViewController.swift
@@ -8,16 +8,120 @@
 
 import UIKit
 
+//サイドのコンテナビューに関するenum
+enum SideStatus {
+    case Opened
+    case Closed
+}
+
+//定数設定などその他
+struct BaseSettings {
+    
+    //ScrollViewのサイズに関するセッテイング
+    static let movedMainX : Int = 280
+    static let closedMainX : Int = 0
+}
+
 class ViewController: UIViewController {
 
+    //メンバ変数
+    var sideStatus : SideStatus! = SideStatus.Closed
+    
+    //Outlet接続した部品一覧
+    @IBOutlet var hiddenButton: UIButton!
+    @IBOutlet var mainContainer: UIView!
+    @IBOutlet var sideContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //コンテナの初期状態を決定
+        self.hiddenButtonSetting(self.sideStatus)
+        
+        //配置したボタンに押した際のアクションを設定する
+        self.hiddenButton.addTarget(self, action: #selector(ViewController.hiddenButtonTapped(_:)), forControlEvents: .TouchUpInside)
     }
 
+    //レイアウト処理が完了した際の処理
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        //AutoLayoutで設定したパーツのX座標・Y座標・幅・高さを再定義する
+        self.mainContainer.frame = CGRectMake(
+            CGFloat(self.mainContainer.frame.origin.x),
+            CGFloat(self.mainContainer.frame.origin.y),
+            CGFloat(self.mainContainer.frame.width),
+            CGFloat(self.mainContainer.frame.height)
+        )
+        self.sideContainer.frame = CGRectMake(
+            CGFloat(self.sideContainer.frame.origin.x),
+            CGFloat(self.sideContainer.frame.origin.y),
+            CGFloat(self.sideContainer.frame.width),
+            CGFloat(self.sideContainer.frame.height)
+        )
+    }
+
+    //ボタンタップ時のメソッド
+    func hiddenButtonTapped(button: UIButton) {
+        self.containerHandlerTapped(SideStatus.Closed)
+    }
+    
+    //コンテナの動きを司るメソッド
+    func containerHandlerTapped(status: SideStatus) {
+        self.hiddenButtonSetting(status)
+        self.judgeSideContainer(status)
+    }
+    
+    //ステータスに応じてメインコンテナの開閉を決定する
+    func judgeSideContainer(status: SideStatus) {
+        
+        if status == SideStatus.Closed {
+            
+            UIView.animateWithDuration(0.2, delay: 0, options: [], animations: {
+                
+                self.mainContainer.frame = CGRectMake(
+                    CGFloat(BaseSettings.closedMainX),
+                    CGFloat(self.mainContainer.frame.origin.y),
+                    CGFloat(self.mainContainer.frame.width),
+                    CGFloat(self.mainContainer.frame.height)
+                )
+                self.hiddenButtonSetting(status)
+                
+            }, completion: nil)
+            
+        } else {
+            
+            UIView.animateWithDuration(0.2, delay: 0, options: [], animations: {
+                
+                self.mainContainer.frame = CGRectMake(
+                    CGFloat(BaseSettings.movedMainX),
+                    CGFloat(self.mainContainer.frame.origin.y),
+                    CGFloat(self.mainContainer.frame.width),
+                    CGFloat(self.mainContainer.frame.height)
+                )
+                self.hiddenButtonSetting(status)
+                
+            }, completion: nil)
+            
+        }
+        
+    }
+    
+    //コンテナ開閉用ボタンの制御を行う
+    func hiddenButtonSetting(status: SideStatus) {
+        
+        if status == SideStatus.Closed {
+            self.hiddenButton.enabled = false
+            self.hiddenButton.alpha = 0
+        } else {
+            self.hiddenButton.enabled = true
+            self.hiddenButton.alpha = 1
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
